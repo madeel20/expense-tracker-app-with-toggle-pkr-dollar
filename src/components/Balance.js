5e31f7cb9431df4a7fc292097f0e5e8a93f78323
Sheikh Muhammad Adeel <smaa.link@gmail.com>
2020-06-21 12:05:08 +0430

    fixed bug

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,14 +5,14 @@ export const Balance = () => {
   const { transactions, sign } = useContext(GlobalContext);
 
   const amounts = transactions.map((transaction) => transaction.amount);
-
-  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  let total=0;
+   total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
 
   return (
     <div>
       <h4>Your Balance</h4>
       <h4>
-        {sign} {total.toFixed(2)}
+        {sign} {parseInt(total).toFixed(2)}
       </h4>
     </div>
   );
